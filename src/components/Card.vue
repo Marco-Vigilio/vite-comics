4aa7e51b2085f0b8db79a29283dab1d9347c3c9a
Marco-Vigilio <tonyvigilio@gmail.com>
2023-06-16 15:32:40 +0200

    creo l'array da AppMain e creo da Card props passando l'array

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <h1>Sono Card</h1>
+        <div>{{ object.series }}</div>
+        <h3>Titolo</h3>
     </div>
 </template>
 <script>
 export default {
     name: "Card",
+    props: {
+        object: Object,
+    },
 }
 </script>
-<style lang="">
+<style lang="" scoped>
     
 </style>
